04c3ada777d451780336c706d972e400dfbd80f0
Jon Su <blair1306@163.com>
2017-09-08 15:47:42 +0800

    Added description and required package information

--- setup.py
+++ setup.py
@@ -1,8 +1,29 @@
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
 from setuptools import setup
 
+description = """
+
+"""
 
 setup(
+    name = 'ADManager',
+    description = 'A simple GUI frontend for android adb',
+    long_description = description,
+    author = 'vmplay',
+    install_requires = [
+        'python-tk',
+    ],
     scripts = [
-        "android_device_manager.py"
-    ]
+        "android_device_manager.py",
+    ],
 )
